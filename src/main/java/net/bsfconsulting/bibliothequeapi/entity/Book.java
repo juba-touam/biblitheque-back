ba165ca92c48c8feeba88c855970509b0e19ab1e
touam <juba.touam@etu.unistra.fr>
2025-02-19 01:23:13 +0100

    wip

--- src/main/java/net/bsfconsulting/bibliothequeapi/entity/Book.java
+++ src/main/java/net/bsfconsulting/bibliothequeapi/entity/Book.java
@@ -20,7 +20,6 @@ public class Book {
     private String author;
     private Integer publicationYear;
     private Boolean available;
-    private String isbn;
     @OneToMany(mappedBy = "book",cascade = CascadeType.ALL,orphanRemoval = true)
     private List<Loan> loans;
 }
